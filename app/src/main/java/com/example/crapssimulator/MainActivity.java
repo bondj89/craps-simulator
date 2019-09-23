3a64890893c43ea28bc1eecb7186c36546b4ef9f
Bond <jobocuban@gmail.com>
2019-09-23 16:45:23 -0600

    Added ListView and tally with dummy data.

--- app/src/main/java/com/example/crapssimulator/MainActivity.java
+++ app/src/main/java/com/example/crapssimulator/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.crapssimulator;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    TextView tally = findViewById(R.id.tally);
+    tally.setText("This will be my win/loss tally line");
+    ListView rolls = findViewById(R.id.rolls);
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.single_roll,
+            getResources().getStringArray(R.array.dummy_rolls));
+    rolls.setAdapter(adapter);
   }
 
   @Override
